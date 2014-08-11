ac4b78c0c57499f5b245766b26607b7780d4785e
Veljko Popovic <v.s.popovic@gmail.com>
2014-08-11 09:48:33 +0200

    moving along

--- index.js
+++ index.js
@@ -3,10 +3,81 @@
 angular.module ('hers.angular',['ui.bootstrap.modal', 'hers.angular.crud']).
 factory ('doubleConfirm', [function () {
   //Use this function as a base function for confirm controllers ....
-  return function($scope, $modalInstance, confirmCancel) {
+  return 
+}])
+.factory('hers.angular.notifications.defaults', [function () {
+    var DEFAULTS = {
+    windowClass: 'hers_angular_notification',
+    size: 'sm',
+    backdrop:true,
+    templateUrl: null
+  };
+  var SET = angular.extend({}, DEFAULTS);
+  return {
+    set : function (settings) {
+      SET = angular.extend({}, DEFAULTS, settings);
+    },
+    get : function () {
+      return SET;
+    }
+  }
+}])
+.factory('hers.angular.notifications', ['$modal','hers.angular.notifications.defaults', function ($modal, defaults) {
+
+  var REGISTRY = {};
+  var LABELS = {
+    'error': 'Error',
+    'warining': 'Warning',
+    'info': 'Info',
+    'success':'Success',
+    'confirm':'Confirm',
+    'dconfirm':'Confirm'
+  };
+
+  var cmap = {
+    'error': 'danger',
+    'warining': 'warning',
+    'info': 'info',
+    'success':'success',
+    'confirm':'danger',
+    'dconfirm':'danger'
+  };
+
+  function Controller($scope, $modalInstance, data, settings) {
+    $scope.allow_cancel = false;
+    $scope.data = data;
+    $scope.settings = angular.extend({
+      label: LABELS[settings.type]
+    }, settings);
+    $scope.tc = cmap[settings.type] || settings.type;
+
+    $scope.ok = function () {
+      $modalInstance.close('ok');
+    }
+  }
+  function CController ($scope, $modalInstance, data, settings) {
+    $scope.allow_cancel = true;
+    $scope.data = data;
+    $scope.settings = angular.extend({
+      label: LABELS[settings.type]
+    }, settings);
+    $scope.tc = cmap[settings.type] || settings.type;
+
+    $scope.ok = function () {
+      $modalInstance.close('ok');
+    }
+
+    $scope.cancel = function () {
+      $modalInstance.close('cancel');
+    }
+  }
+
+  function DCController ($scope, $modalInstance, data, settings) {
     $scope.step = 0;
     $scope.state = null;
+    $scope.allow_cancel = true;
 
+    ///pokusaj ovo da provuces zezajuci se sa settings.string ...
     function dodado (state) {
       if ($scope.step === 0) { 
         $scope.step+=1; 
@@ -40,57 +111,27 @@ factory ('doubleConfirm', [function () {
       $modalInstance.close('cancel');
     };
   }
-}])
-.factory('hers.angular.notifications.defaults', [function () {
-    var DEFAULTS = {
-    windowClass: 'hers_angular_notification',
-    size: 'sm',
-    backdrop:true,
-    templateUrl: null
-  };
-  var SET = angular.extend({}, DEFAULTS);
-  return {
-    set : function (settings) {
-      SET = angular.extend({}, DEFAULTS, settings);
-    },
-    get : function () {
-      return SET;
-    }
-  }
-}])
-.factory('hers.angular.notifications', ['$modal','hers.angular.notifications.defaults', function ($modal, defaults) {
-
-  var REGISTRY = {};
-  var LABELS = {
-    'error': 'Error',
-    'warining': 'Warning',
-    'info': 'Info',
-    'success':'Success'
-  };
-
-  var cmap = {
-    'error': 'danger',
-    'warining': 'warning',
-    'info': 'info',
-    'success':'success'
-  };
-
-  function Controller($scope, $modalInstance, data, settings) {
-    $scope.data = data;
-    $scope.settings = angular.extend({
-      label: LABELS[settings.type]
-    }, settings);
-    $scope.tc = cmap[settings.type] || settings.type;
-
-    $scope.ok = function () {
-      $modalInstance.dismiss('ok');
-    }
-  }
   
   function launch (id, data) {
-    var settings = REGISTRY[id];
+    var rec = REGISTRY[id];
+    if (!rec) return; //za sad?!?! TODO
+
     var c = angular.extend({}, defaults.get(), settings.modal);
-    c.controller = Controller;
+    switch (rec.type) {
+      case 'error':
+      case 'warning':
+      case 'info':
+      case 'success':
+        c.controller = Controller;
+        break;
+      case 'confirm':
+        controller = CController;
+        break;
+      case 'dconfirm':
+        c.controller = DCController;
+        break;
+      default: return; //TODO think of something bit smarter
+    }
 
     c.resolve = {
       settings: function () {return {
@@ -99,7 +140,6 @@ factory ('doubleConfirm', [function () {
       };},
       data:function () {return data;}
     }
-
     return $modal.open (c);
   }
   return {
